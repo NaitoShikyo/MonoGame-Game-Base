aa636af02216a33ae434bd30fc34a2dc3ee6e7d8
Adam Dorling <Naitoshikyox@gmail.com>
2015-06-30 00:54:08 +0100

    Ignoring the ignore so it may work better

---
+++ TestMonogame/MagicCard.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TestMonogame;
+
+
+
+namespace MTGGame
+{
+    #region Enums 
+    enum TapState
+    {
+        Untapped,
+        Tapped,
+    }
+    #endregion
+
+    #region Gamethings
+
+    #region GameScreen
+
+    #endregion
+
+    #region TheStack
+
+    public class Stack
+    {
+        public List<IMagicEffect> Thing;
+        private Stack()
+        {
+
+        }
+
+        public static Stack()
+        {
+            
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Magic Card Class
+
+    class Card
+    {
+    }
+
+    #endregion
+
+
+
+    #region Interfaces
+
+    /// <summary>
+    /// All effects (ie shroud) will inherit off this Interface
+    /// </summary>
+    interface IMagicEffect
+    {
+
+    }
+
+
+   
+
+    #endregion
+
+}
